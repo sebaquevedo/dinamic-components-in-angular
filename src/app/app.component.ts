adaeccfb0841a797c5541fc327d3f010ee42e6f9
Sebastianm <sebaqd91@gmail.com>
2024-08-21 19:05:49 +0100

    destroy component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,38 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  ComponentRef,
+  inject,
+  viewChild,
+  ViewContainerRef,
+} from "@angular/core";
+import { WidgetComponent } from "./widget/widget.component";
 @Component({
   selector: "app-root",
   standalone: true,
   template: `
     <img class="logo" src="./logo.svg" alt="Decoded Frontend" />
     <h1 class="page-title">Dynamic Components</h1>
-    
+
     <main id="content">
+      <ng-container #container></ng-container>
       <section class="toolbar">
-        <button (click)="createComponent()" class="create">Create Component</button>
-        <button (click)="destroyComponent()" class="destroy">Destroy Component</button>
+        <button (click)="createComponent()" class="create">
+          Create Component
+        </button>
+        <button (click)="destroyComponent()" class="destroy">
+          Destroy Component
+        </button>
       </section>
     </main>
   `,
 })
 export class AppComponent {
-  createComponent() {}
-  destroyComponent() {}
+  vcr = viewChild("container", { read: ViewContainerRef });
+  #componentRef?: ComponentRef<WidgetComponent>;
+  createComponent() {
+    this.#componentRef = this.vcr()?.createComponent(WidgetComponent);
+  }
+  destroyComponent() {
+    this.#componentRef?.destroy();
+  }
 }
