1efaf1daebd9935c5f954b4bdad32e55e613324e
Sebastianm <sebaqd91@gmail.com>
2024-08-21 20:40:05 +0100

    content projection on dinamic component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
 import {
   Component,
   ComponentRef,
+  ElementRef,
   inject,
+  TemplateRef,
   viewChild,
   ViewContainerRef,
 } from "@angular/core";
 import { WidgetComponent } from "./widget/widget.component";
+import { WeatherContentComponent } from "./widget/weather-content.component";
 @Component({
   selector: "app-root",
   standalone: true,
   template: `
     <img class="logo" src="./logo.svg" alt="Decoded Frontend" />
     <h1 class="page-title">Dynamic Components</h1>
-
+    <ng-template #content>
+      <app-weather-content />
+    </ng-template>
     <main id="content">
       <ng-container #container></ng-container>
       <section class="toolbar">
@@ -25,14 +30,25 @@ import { WidgetComponent } from "./widget/widget.component";
       </section>
     </main>
   `,
+  imports: [WeatherContentComponent],
 })
 export class AppComponent {
   vcr = viewChild("container", { read: ViewContainerRef });
+  content = viewChild<TemplateRef<unknown>>("content");
   #componentRef?: ComponentRef<WidgetComponent>;
   createComponent() {
-    this.#componentRef = this.vcr()?.createComponent(WidgetComponent);
+    this.vcr()?.clear();
+    const contentView = this.vcr()?.createEmbeddedView(this.content()!);
+    this.#componentRef = this.vcr()?.createComponent(WidgetComponent, {
+      projectableNodes: [contentView?.rootNodes!],
+    });
+    this.#componentRef?.setInput("title", "Weather");
+    this.#componentRef?.setInput("description", "Currently in Vienna");
+    this.#componentRef?.instance.closed.subscribe(() =>
+      this.#componentRef?.destroy()
+    );
   }
   destroyComponent() {
-    this.#componentRef?.destroy();
+    this.vcr()?.clear();
   }
 }
